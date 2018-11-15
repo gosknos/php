76b7c163a71616eefe1265838f55c00c18ff7ff5
gosknos <gxcharlotte@gmail.com>
2018-11-15 16:46:43 +0100

    point

--- index.php
+++ index.php
@@ -18,28 +18,43 @@ $filename = 'friends.txt';
 $name = $_POST['name'];
 $nameFilter = $_POST['nameFilter'];
 $file = fopen( $filename, "r" );
+echo "<ul>";
 $file2 = fopen( $filename, "r" );
 
 while (!feof($file)) {
     if ($nameFilter != NULL){
         if (strstr(fgets($file), "$nameFilter", false) != NULL){
             $ligne = fgets($file2)."<br/>";
-            echo $ligne;
+            echo "<li>$ligne</li>";
         }
         else {
             fgets($file2);
         }
     }
-    else { echo fgets($file)."<br/>";}
+    else {
+        echo fgets($file)."<br/>";
+    }
 }
 
-// appending to file
-$file = fopen( $filename, "a+" );
-if ($file != false)
-{
-    echo "<b>$name</b>";
-    fwrite($file,"$name\n");
-    fclose($file);
+if ($file != false) {
+    while (!feof($file)) {
+        $friend = fgets($file);
+        if(strlen($friend) > 0){
+            echo "<li>$friend</li>";
+        }
+    }
 }
+fclose($file);
+
+if (isset($_POST['name']) && strlen($_POST['name']) > 0) {
+    $name = $_POST['name'];
+    $file = fopen( $filename, "a" );
+    if($file != false) {
+        fwrite( $file, $name . "\n" );
+        echo "<b><li>$name</li></b>";
+        fclose($file);
+    }
+}
+echo "</ul>";
 include('footer.html');
 ?>
